9e528104295ffa276c90f3a512580462bd05ea00
Mathias Grimm <mathiasgrimm@gmail.com>
2026-04-07 08:56:53 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return [
+        'request()->ip()' => request()->ip(),
+        'request()->ips()' => request()->ips(),
+    ];
 });
